e57ad182e29db6242f4e3e339a74ea2ed236dc3d
Alejandro F <afuste7@gmail.com>
2020-02-02 18:15:45 -0500

    changed input element to textarea element, and added margin and padding to hour

--- script.js
+++ script.js
@@ -8,21 +8,38 @@ $(document).ready(function() {
 	console.log(moment());
 
 	let hours = [ '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm' ];
+	let dataHours = [ 9, 10, 11, 12, 1, 2, 3, 4, 5 ];
+	let container = $('.container');
 	let timeBlock = $('<div>');
 	timeBlock.addClass('time-block');
 	console.log(timeBlock);
 
-	// function renderRows() {
-	// 	timeBlock.empty();
+	function renderRows() {
+		timeBlock.empty();
 
-	// 	for (i = 0; i < hours.length; i++) {
-	//         let rowDiv = $('<div>');
-	//         rowDiv.addClass('row');
+		for (i = 0; i < hours.length; i++) {
+			let rowDiv = $('<div>');
+			rowDiv.addClass('row');
 
-	//         rowDiv.append(span, input, button);
-	// 		timeBlock.append(rowDiv);
-	// 	}
-	// }
+			let span = $('<span>');
+			span.addClass('hour');
 
-	// renderRows();
+			span.attr('data-hour', dataHours[i]);
+			span.text(hours[i]);
+
+			let textarea = $('<textarea>');
+			textarea.addClass('time-block');
+			textarea.attr('cols', '100');
+
+			let button = $('<button>');
+			button.addClass('saveBtn');
+			button.attr('type', 'submit');
+
+			rowDiv.append(span, textarea, button);
+			// timeBlock.append(rowDiv);
+			container.append(rowDiv);
+		}
+	}
+
+	renderRows();
 });
