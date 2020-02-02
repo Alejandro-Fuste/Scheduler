183c5590dcab6288b62920b54dab0ba787b9323c
Alejandro F <afuste7@gmail.com>
2020-02-02 14:44:39 -0500

    added current day & dateto header jumbotron

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+	m = moment().format('dddd, MMMM Do YYYY');
+
+	let currentDayDiv = $('#currentDay');
+
+	currentDayDiv.append(m);
+
 	console.log(moment());
+	console.log(m);
+	console.log(currentDayDiv);
 });
