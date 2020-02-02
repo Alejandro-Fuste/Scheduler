a9643f0cb8e875fcd12813b253189863289d15c0
Alejandro F <afuste7@gmail.com>
2020-02-02 15:45:02 -0500

    I added time block container div and added class to container. I also added the first parts of the function for adding the rows for the times.

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ $(document).ready(function() {
 	currentDayDiv.append(m);
 
 	console.log(moment());
-	console.log(m);
-	console.log(currentDayDiv);
+
+	let hours = [ '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm' ];
+	let timeBlock = $('<div>');
+	timeBlock.addClass('time-block');
+	console.log(timeBlock);
+
+	// function renderRows() {
+	// 	timeBlock.empty();
+
+	// 	for (i = 0; i < hours.length; i++) {
+	//         let rowDiv = $('<div>');
+	//         rowDiv.addClass('row');
+
+	//         rowDiv.append(span, input, button);
+	// 		timeBlock.append(rowDiv);
+	// 	}
+	// }
+
+	// renderRows();
 });
